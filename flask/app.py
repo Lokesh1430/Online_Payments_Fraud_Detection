2ce8a2d31b8ec3546ce6657b770854fc8a4963f4
SMART LOKESH <109786176+Lokesh1430@users.noreply.github.com>
2026-02-28 11:18:32 +0530

    Add files via upload

---
+++ flask/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+# Load model
+model = pickle.load(open(r"C:\Online Payments Fraud Detection\flask\payments.pkl", 'rb'))
+
+app = Flask(__name__)
+
+# Home Page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# Predict Page
+@app.route("/predict")
+def predict_page():
+    return render_template("predict.html")
+
+# Prediction Route
+@app.route("/submit", methods=['POST'])
+def submit():
+
+    data = list(request.form.values())
+    data = [float(i) for i in data]
+
+    # add missing feature
+    data.append(0)
+
+    x = np.array([data])
+
+    pred = model.predict(x)
+
+    # ⭐ CONVERT 0/1 → TEXT LABEL
+    if pred[0] == 1:
+        result = "is Fraud"
+    else:
+        result = "is not Fraud"
+
+    return render_template("submit.html", prediction_text=result)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
